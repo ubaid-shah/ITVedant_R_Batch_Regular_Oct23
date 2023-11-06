b61d8601fd8a0d940d4237f14109f57918de14d4
Ubaid Shah <47923019+ubaid-shah@users.noreply.github.com>
2023-11-06 18:34:17 +0530

    Add files via upload

---
+++ lect2.R
@@ -0,0 +1,135 @@
+x<-10
+y<-20
+z<-x+y
+
+# Mathematical Operators  + - / * ^ %% %/%
+
+add<-x+y
+sub<-z-y
+div=z/sub
+mul=div*5
+exp_1=5^2
+mod=mul%%exp_1
+floor_div=9%/%2
+
+print(paste(add,"\n",sub,"\n",div,"\n"))
+
+print(paste(add,"\n",sub,"\n",div,"\n",end=' '))
+
+cat(add,"\n",sub,"\n",div,"\n")
+
+
+
+message="Welcome"
+name="Nikhil"
+
+#print((message,name))
+print(paste0(message,name))
+
+
+paste0(message,name)
+
+paste(message,name)
+
+print(z)
+cat(z)
+
+
+
+cat(message,name,sep='')
+
+class(message)
+typeof(message)
+
+
+class(x)
+typeof(x)
+
+
+s=1.5
+typeof(s)
+class(s)
+
+t=10
+typeof(t)
+class(t)
+
+
+t_int=as.integer(t)
+typeof(t_int)
+
+
+t_int2=100L
+t_int2*2
+typeof(t_int2)
+class(t_int2)
+
+
+class(1)
+class(TRUE)
+typeof(TRUE)
+
+
+# Comparison Operators > < <= >= != ==
+
+5>3
+6<5
+6>=8
+class(6<=8)
+typeof(6!=8)
+
+
+# assignment operators <- left to right  ->  right to left    = normal
+21->b
+b
+
+
+
+# Logical Opertaors & and  | or  ! not
+
+TRUE & TRUE
+TRUE & FALSE
+
+TRUE | TRUE
+TRUE | FALSE
+
+!TRUE
+
+TRUE & TRUE & FALSE
+TRUE && TRUE && FALSE
+
+TRUE || TRUE || FALSE
+
+
+(6>5) && (5>10)
+
+# Numeric
+
+x="10"
+y="100L"
+z="50Q"
+
+x=as.integer(x)
+y=as.integer(y)
+
+z=as.integer(z)
+
+z=50+45i
+
+typeof(z)
+class(z)
+
+z=as.integer(z)
+z
+z=as.complex(z)
+
+
+
+
+
+
+#k="100"L
+
+
+
+
---
+++ lect2_2.R
@@ -0,0 +1,14 @@
+var1=readline("Enter value1: ")
+var2=readline("Enter value2: ")
+var3=readline("Enter value3: ")
+
+cat(var3,"\n","Addition of two values: ",var1+var2)
+
+{
+  var1=readline("Enter value1: ")
+  var2=readline("Enter value2: ")
+  var3=readline("Enter value3: ")
+}
+
+var1=as.numeric(readline());var2=as.numeric(readline());var3=as.integer(readline())
+
