d827f707e9048135c23f63b7fb6db1804b224ad8
Ubaid Shah <47923019+ubaid-shah@users.noreply.github.com>
2023-12-21 11:45:45 +0530

    Add files via upload

--- EDA_Bigmart.R
+++ EDA_Bigmart.R
@@ -36,4 +36,50 @@ colSums(is.na(df))
 df$Item_Weight<-ifelse((df$Item_Weight=='Regular'|df$Item_Weight=='Low Fat'),0,df$Item_Weight)
 df$Item_Weight<-as.numeric(df$Item_Weight)
 df$Item_Weight<-replace(df$Item_Weight,is.na(df$Item_Weight),mean(df$Item_Weight,na.rm = T))
-unique(df$Item_Weight)
+
+View(df[df$Item_Weight==0,])
+df<-(df[!df$Item_Weight==0,])
+
+df$Item_Outlet_Sales<-as.numeric(df$Item_Outlet_Sales)
+df$Outlet_Establishment_Year<-as.numeric(df$Outlet_Establishment_Year)
+df$Item_MRP<-as.numeric(df$Item_MRP)
+df$Item_Visibility<-as.numeric(df$Item_Visibility)
+
+summary(df)
+View(df)
+
+
+table(df$Outlet_Size)
+df$Outlet_Size<-ifelse(df$Outlet_Size=='','Medium',df$Outlet_Size)
+View(df[df$Outlet_Size=='',])
+
+
+table(df$Item_Fat_Content)
+
+View(table(df$Item_Type))
+
+df_IT<-data.frame(table(df$Item_Type))
+df %>% select(2:4) %>% glimpse()
+
+View(df_IT)
+df_IT$Freq
+P_item_type<-ggplot(df_IT,aes(x='',y=Freq,fill=Var1))+
+  geom_col()+
+  geom_text(aes(label=paste(round(Freq*100/sum(Freq)),'%')),position = position_stack(vjust = 0.2))+
+  coord_polar(theta = 'y')+
+  labs(title='% of Item_type')+
+  theme_void()
+
+
+P_item_type
+
+
+
+
+
+
+
+
+
+
+
